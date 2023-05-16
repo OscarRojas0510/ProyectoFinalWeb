2cd95693ef7b2c56c3059893ecccfba65e10ce95
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2023-05-16 04:56:25 -0600

    Conexión entre el carrito y catalogo

--- catProd.php
+++ catProd.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require 'consultas.php';
 $consultas = new Consultas();
 $datos;
@@ -25,8 +26,11 @@ if ($isEmpty) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
     <link rel="icon" href="img/mainImage.svg" type="image/x-icon">
     <link rel="stylesheet" href="css/estilosCatProd.css">
     <link rel="stylesheet" href="css/animate.css">
@@ -36,47 +40,13 @@ if ($isEmpty) {
 </head>
 
 <body class="container-fluid">
-    <header id="header" class="row d-flex ">
-        <div class="col-2 offset-1 h-100 d-flex justify-content-center align-items-center">
-            <img src="img/mainImage.svg" alt="" class="h-100">
-        </div>
-        <div class="col-8 p-0 d-flex align-items-center">
-            <p class="w-100 m-0 text-left font-weight-bold" id="tituloPgina">
-                Catálogo de Productos
+    <?php
 
-            </p>
-        </div>
-    </header>
-    <nav class="navbar navbar-expand-lg navbar-light row p-0" id="navBar">
-        <button class="navbar-toggler offset-11 " type="button" data-toggle="collapse"
-            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse col-12 p-0 justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Iniciar Sesión</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">
-                        Registro
-                    </a>
-                </li>
-                <li class="d-flex">
-                    <img src="img/carrito.png" class="align-self-center" alt="">
-                </li>
-                <li class="nav-item dropdown active">
-                    <img src="img/options.png" class="align-self-center p-1 nav-link dropdown-toggle" alt=""
-                        role="button" data-toggle="dropdown">
-                    <div class="dropdown-menu dropdown-menu-right p-0 m-0 rounded-0" id="menuDesplegable">
-                        <a class="dropdown-item" href="#">Catálogo de productos</a>
-                    </div>
-                </li>
 
-            </ul>
-        </div>
-    </nav>
+    include("navbar.php");
+    include("modal.php");
+
+    ?>
     <section class="row d-flex flex-row align-items-center align-content-center p-0" id="section1">
         <div class="col-2 offset-1  align-self-stretch d-flex flex-row justify-content-start p-0">
             <p class=" align-self-center m-0 font-weight-bold">
@@ -163,22 +133,13 @@ if ($isEmpty) {
             }
             ?>
         </div>
-        <form id="myForm" action="index.php" method="POST">
-            <input type="hidden" id="divIdInput" name="divId">
+        <form id="myForm" action="detalle.php" method="GET">
+            <input type="hidden" id="divIdInput" name="articulo">
         </form>
     </main>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/estilosCatProd.css">
     <script src="js/scriptsCatProd.js"></script>
 </body>
--- navbar.php
+++ navbar.php
@@ -1,41 +1,91 @@
-<?php 
+<?php
 
-if(isset($_SESSION['carrito'])){
-$carrito_mio=$_SESSION['carrito'];
+if (isset($_SESSION['carrito'])) {
+  $carrito_mio = $_SESSION['carrito'];
 }
 
 // contamos nuestro carrito
-if(isset($_SESSION['carrito'])){
-    for($i=0;$i<=count($carrito_mio)-1;$i ++){
-        if(isset($carrito_mio[$i])){
-        if($carrito_mio[$i]!=NULL){ 
-        if(!isset($carrito_mio['cantidad'])){$carrito_mio['cantidad'] = '0';}else{ $carrito_mio['cantidad'] = $carrito_mio['cantidad'];}
+if (isset($_SESSION['carrito'])) {
+  for ($i = 0; $i <= count($carrito_mio) - 1; $i++) {
+    if (isset($carrito_mio[$i])) {
+      if ($carrito_mio[$i] != NULL) {
+        if (!isset($carrito_mio['cantidad'])) {
+          $carrito_mio['cantidad'] = '0';
+        } else {
+          $carrito_mio['cantidad'] = $carrito_mio['cantidad'];
+        }
         $total_cantidad = $carrito_mio['cantidad'];
-    $total_cantidad ++ ;
-    if(!isset($totalcantidad)){$totalcantidad = '0';}else{ $totalcantidad = $totalcantidad;}
-    $totalcantidad += $total_cantidad;
-    }}}
+        $total_cantidad++;
+        if (!isset($totalcantidad)) {
+          $totalcantidad = '0';
+        } else {
+          $totalcantidad = $totalcantidad;
+        }
+        $totalcantidad += $total_cantidad;
+      }
+    }
+  }
 }
 
-    //declaramos variables
-     if(!isset($totalcantidad)){$totalcantidad = '';}else{ $totalcantidad = $totalcantidad;}
+//declaramos variables
+if (!isset($totalcantidad)) {
+  $totalcantidad = '';
+} else {
+  $totalcantidad = $totalcantidad;
+}
 
 ?>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+  integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="css/estilosNavbar.css">
+<header id="header" class="row d-flex ">
+  <div class="col-2 offset-1 h-100 d-flex justify-content-center align-items-center">
+    <img src="img/mainImage.svg" alt="" class="h-100">
+  </div>
+  <div class="col-8 p-0 d-flex align-items-center">
+    <p class="w-100 m-0 text-left font-weight-bold" id="tituloPgina">
+      Catálogo de Productos
 
+    </p>
+  </div>
+</header>
 <!-- NAVBAR -->
-<nav class="navbar navbar-expand-lg navbar-dark " style="background-color: pink;">
-<div class="container-fluid">
-    <a class="navbar-brand" href="#"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modal_cart" style="color: red; cursor:pointer;"><i class="fas fa-shopping-cart"></i> <?php echo $totalcantidad; ?></a>
-        </li>
-      </ul>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light row p-0" id="navBar">
+  <button class="navbar-toggler offset-11 " type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse col-12 p-0 justify-content-end" id="navbarSupportedContent">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Iniciar Sesión</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">
+          Registro
+        </a>
+      </li>
+      <li class="d-flex">
+        <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modal_cart" style="color: black; cursor:pointer;">
+          <img src="img/carrito.png">
+          <?php echo $totalcantidad; ?>
+        </a>
+      </li>
+      <li class="nav-item dropdown active">
+        <img src="img/options.png" class="align-self-center p-1 nav-link dropdown-toggle" alt="" role="button"
+          data-toggle="dropdown">
+        <div class="dropdown-menu dropdown-menu-right p-0 m-0 rounded-0" id="menuDesplegable">
+          <a class="dropdown-item" href="catProd.php">Catálogo de productos</a>
+        </div>
+      </li>
+
+    </ul>
   </div>
 </nav>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+  integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <!-- END NAVBAR -->
